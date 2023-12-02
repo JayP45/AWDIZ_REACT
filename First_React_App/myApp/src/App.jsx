bf1d48073ef99460ec9e17d501989ae56b526bd8
Jay Patil <122766420+JayP45@users.noreply.github.com>
2023-12-03 00:02:08 +0530

    updates

--- First_React_App/myApp/src/App.jsx
+++ First_React_App/myApp/src/App.jsx
@@ -1,19 +1,26 @@
-import React,{ useEffect, useState } from 'react'
-
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from './Components/Home';
+import Register from './Components/Register';
+import Login from './Components/Login';
+import Navbar from './Components/Navbar';
+import NoPageFound from './Components/NoPageFound';
+import Footer from './Components/Footer';
 const App = () => {
-  const [count,setCount] = useState(0)
-
-  useEffect(()=>{
-    setTimeout(()=>{
-      alert('hello i am alert')
-    },5000)
-  })
   return (
     <>
-      {count}
-      <button onClick={()=>setCount(count+1)}>Click me</button>
-      {count}
-      <button onClick={()=>setCount(count-1)}>Click me</button>
+      <div className="App">
+        <Router>
+        <Navbar />
+          <Routes>
+            <Route  path='*' element={<NoPageFound />} />
+            <Route exact path='/' element={<Home />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
+          </Routes>
+        </Router>
+        <Footer />
+      </div>
     </>
   )
 }
